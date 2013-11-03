f87bedb6ffa5c7ef851ea1bd359ecd9fded0e373
Mikhail Kuznetsov <kuznetsov.mikhail@otr.ru>
2013-11-03 17:24:37 +0400

    response tag

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -6,11 +6,16 @@ class ClientsController < ApplicationController
   soap_action "CheckClient",
               :args   => :string,
               :return => :string,
-              :to     => :check_client
+              :to     => :check_client,
+              :response_tag => "CheckClientResponse"
+
   def check_client
     Rails.logger.debug params[:value]
     soap_response = "1;2334;Ok"
     render :soap => soap_response
+    #Rails.logger.debug params[:response.to_s]
+    #headers["Content-Length"]
+    #.length.to_s
   end
 
   def dump_parameters
